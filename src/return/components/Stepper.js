30b7d9cbe2aca47d7a571184b7ecae0bb4afe2c2
Phan Hương <phanngochuong@hotmail.com>
2023-04-09 18:48:49 +0200

    Update Stepper.js

--- src/return/components/Stepper.js
+++ src/return/components/Stepper.js
@@ -17,212 +17,203 @@ const ColorlibConnector = styled(StepConnector)(({ theme }) => ({
     [`&.${stepConnectorClasses.alternativeLabel}`]: {
       top: 15,
     },
-  [`&.${stepConnectorClasses.active}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      background:
-        '#a7d9fd',
+    [`&.${stepConnectorClasses.active}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            background: '#a7d9fd',
+        },
     },
-  },
-  [`&.${stepConnectorClasses.completed}`]: {
-    [`& .${stepConnectorClasses.line}`]: {
-      background:
-        '#a7d9fd',
+    [`&.${stepConnectorClasses.completed}`]: {
+        [`& .${stepConnectorClasses.line}`]: {
+            background: '#a7d9fd',
+        },
     },
-  },
-  [`& .${stepConnectorClasses.line}`]: {
-    height: 3,
-    border: 0,
-    backgroundColor:
-      theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
-    borderRadius: 1,
-  },
+    [`& .${stepConnectorClasses.line}`]: {
+        height: 3,
+        border: 0,
+        backgroundColor:
+        theme.palette.mode === 'dark' ? theme.palette.grey[800] : '#eaeaf0',
+        borderRadius: 1,
+    },
 }));
 
 const ColorlibStepIconRoot = styled('div')(({ theme, ownerState }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
-  zIndex: 1,
-  color: '#fff',
-  width: 30,
-  height: 30,
-  display: 'flex',
-  borderRadius: '50%',
-  justifyContent: 'center',
-  alignItems: 'center',
-  ...(ownerState.active && {
-    background:
-      '#a7d9fd',
-  boxShadow: '0 2px 4px 0 rgba(0,0,0,.26)',
-  }),
-  ...(ownerState.completed && {
-    background:
-      '#a7d9fd',
-  }),
+    backgroundColor: theme.palette.mode === 'dark' ? theme.palette.grey[700] : '#ccc',
+    zIndex: 1,
+    color: '#fff',
+    width: 30,
+    height: 30,
+    display: 'flex',
+    borderRadius: '50%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    ...(ownerState.active && {
+        background: '#a7d9fd',
+        boxShadow: '0 2px 4px 0 rgba(0,0,0,.26)',
+    }),
+    ...(ownerState.completed && {
+        background: '#a7d9fd',
+    }),
 }));
 
 function ColorlibStepIcon(props) {
-  const { active, completed, className } = props;
+    const { active, completed, className } = props;
 
-  const icons = {
-    1: <SettingsIcon sx={{ fontSize: 18, color : '#333333' }} />,
-    2: <GroupAddIcon sx={{ fontSize: 18, color : '#333333' }} />,
-    3: <VideoLabelIcon sx={{ fontSize: 18, color : '#333333' }} />,
-    4: <VideoLabelIcon sx={{ fontSize: 18, color : '#333333' }} />,
-  };
+    const icons = {
+        1: <SettingsIcon sx={{ fontSize: 18, color : '#333333' }} />,
+        2: <GroupAddIcon sx={{ fontSize: 18, color : '#333333' }} />,
+        3: <VideoLabelIcon sx={{ fontSize: 18, color : '#333333' }} />,
+        4: <VideoLabelIcon sx={{ fontSize: 18, color : '#333333' }} />,
+    };
 
-  return (
-    <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
-      {icons[String(props.icon)]}
-    </ColorlibStepIconRoot>
-  );
+    return (
+        <ColorlibStepIconRoot ownerState={{ completed, active }} className={className}>
+            {icons[String(props.icon)]}
+        </ColorlibStepIconRoot>
+    );
 }
 
 ColorlibStepIcon.propTypes = {
-  /**
-   * Whether this step is active.
-   * @default false
-   */
-  active: PropTypes.bool,
-  className: PropTypes.string,
-  /**
-   * Mark the step as completed. Is passed to child components.
-   * @default false
-   */
-  completed: PropTypes.bool,
-  /**
-   * The label displayed in the step icon.
-   */
-  icon: PropTypes.node,
+    /**
+     * Whether this step is active.
+     * @default false
+     */
+    active: PropTypes.bool,
+    className: PropTypes.string,
+    /**
+     * Mark the step as completed. Is passed to child components.
+     * @default false
+     */
+    completed: PropTypes.bool,
+    /**
+     * The label displayed in the step icon.
+     */
+    icon: PropTypes.node,
 };
 
 const steps = ['Step 1', 'Step 2', 'Step 3', 'Step 4'];
 
 const ReturnStepper = () => {
 
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [skipped, setSkipped] = React.useState(new Set());
 
-  const isStepOptional = (step) => {
-    return step === 1;
-  };
+    const isStepOptional = (step) => {
+        return step === 1;
+    };
 
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
+    const isStepSkipped = (step) => {
+        return skipped.has(step);
+    };
 
-  const handleNext = () => {
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
-    }
+    const handleNext = () => {
+        let newSkipped = skipped;
+        if (isStepSkipped(activeStep)) {
+            newSkipped = new Set(newSkipped.values());
+            newSkipped.delete(activeStep);
+        }
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setSkipped(newSkipped);
+    };
 
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
+    const handleBack = () => {
+        setActiveStep((prevActiveStep) => prevActiveStep - 1);
+    };
 
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+    const handleSkip = () => {
+        if (!isStepOptional(activeStep)) {
+            throw new Error("You can't skip a step that isn't optional.");
+        }
+        setActiveStep((prevActiveStep) => prevActiveStep + 1);
+        setSkipped((prevSkipped) => {
+            const newSkipped = new Set(prevSkipped.values());
+            newSkipped.add(activeStep);
+            return newSkipped;
+        });
+    };
 
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
-    }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
+    const handleReset = () => {
+        setActiveStep(0);
+    };
   
-  const getStepContent = (step) => {
-    switch (step) {
-        case 0:
-        return (<>
+    const getStepContent = (step) => {
+        switch (step) {
+            case 0:
+                return (<>
                     <h1>First step</h1>
                 </>);
-        case 1:
-          return (<>
+            case 1:
+                return (<>
                     <h1>Second step</h1>
                 </>);
-        case 2:        
-          return (<>
+            case 2:        
+                return (<>
                     <h1>Third step</h1>
                 </>);         
-        case 3:        
-          return (<>
+            case 3:        
+                return (<>
                     <h1>Fourth step</h1>
-                  </>);       
-        default:
-        return 'Unknown step';
+                </>);       
+            default:
+            return 'Unknown step';
+        }
     }
-  }
   
   return (
     <Stack sx={{ width: '100%' }} spacing={4}>
-      <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
-        {steps.map((label, index) => {
-            const stepProps = {};
-            const labelProps = {};
-            if (isStepOptional(index)) {
-              labelProps.optional = (
-                <Typography variant="caption">Optional</Typography>
-              );
-            }
-            if (isStepSkipped(index)) {
-              stepProps.completed = false;
-            }
-            return (
-              <Step key={label} {...stepProps}>
-                <StepLabel StepIconComponent={ColorlibStepIcon} {...labelProps}></StepLabel>
-              </Step>
-            );
-        })}
+        <Stepper alternativeLabel activeStep={activeStep} connector={<ColorlibConnector />}>
+            {steps.map((label, index) => {
+                const stepProps = {};
+                const labelProps = {};
+                if (isStepOptional(index)) {
+                    labelProps.optional = (
+                        <Typography variant="caption">Optional</Typography>
+                    );
+                }
+                if (isStepSkipped(index)) {
+                    stepProps.completed = false;
+                }
+                return (
+                    <Step key={label} {...stepProps}>
+                        <StepLabel StepIconComponent={ColorlibStepIcon} {...labelProps}></StepLabel>
+                    </Step>
+                );
+            })}
+        </Stepper>
 
-      </Stepper>
-      {activeStep === steps.length ? (
-        <React.Fragment>
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            All steps completed - you&apos;re finished
-          </Typography>
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Box sx={{ flex: '1 1 auto' }} />
-            <Button onClick={handleReset}>Reset</Button>
-          </Box>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          {getStepContent(activeStep)}
-          <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
-            <Button
-              color="inherit"
-              disabled={activeStep === 0}
-              onClick={handleBack}
-              sx={{ mr: 1 }}
-            >
-              Back
-            </Button>
-            <Box sx={{ flex: '1 1 auto' }} />
-            {isStepOptional(activeStep) && (
-              <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
-                Skip
-              </Button>
-            )}
-
-            <Button onClick={handleNext}>
-              {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
-            </Button>
-          </Box>
-        </React.Fragment>
-      )}
+        {activeStep === steps.length ? (
+            <React.Fragment>
+                <Typography sx={{ mt: 2, mb: 1 }}>
+                    All steps completed - you&apos;re finished
+                </Typography>
+                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                    <Box sx={{ flex: '1 1 auto' }} />
+                    <Button onClick={handleReset}>Reset</Button>
+                </Box>
+            </React.Fragment>
+        ) : (
+            <React.Fragment>
+                {getStepContent(activeStep)}
+                <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
+                    <Button
+                        color="inherit"
+                        disabled={activeStep === 0}
+                        onClick={handleBack}
+                        sx={{ mr: 1 }}
+                    >
+                        Back
+                    </Button>
+                    <Box sx={{ flex: '1 1 auto' }} />
+                    {isStepOptional(activeStep) && (
+                        <Button color="inherit" onClick={handleSkip} sx={{ mr: 1 }}>
+                            Skip
+                        </Button>
+                    )}
+                    <Button onClick={handleNext}>
+                        {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                    </Button>
+                </Box>
+            </React.Fragment>
+        )}
     </Stack>
   );
 }
